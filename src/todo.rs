e6c37224c70355584aa2b5dab3b446e8d5c2297d
Nick Fitzgerald <fitzgen@gmail.com>
2019-04-04 12:48:12 -0700

    Initial commit

---
+++ src/todo.rs
@@ -0,0 +1,173 @@
+//! Type definition and `dodrio::Render` implementation for a single todo item.
+
+use crate::keys;
+use dodrio::{Node, Render, RenderContext, RootRender, VdomWeak};
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+use wasm_bindgen::{prelude::*, JsCast};
+
+/// A single todo item.
+#[derive(Serialize, Deserialize)]
+pub struct Todo<C> {
+    id: usize,
+    title: String,
+    completed: bool,
+
+    #[serde(skip)]
+    edits: Option<String>,
+
+    #[serde(skip)]
+    _controller: PhantomData<C>,
+}
+
+/// Actions on a single todo item that can be triggered from the UI.
+pub trait TodoActions {
+    /// Toggle the completion state of the todo item with the given id.
+    fn toggle_completed(root: &mut dyn RootRender, vdom: VdomWeak, id: usize);
+
+    /// Delete the todo item with the given id.
+    fn delete(root: &mut dyn RootRender, vdom: VdomWeak, id: usize);
+
+    /// Begin editing the todo item with the given id.
+    fn begin_editing(root: &mut dyn RootRender, vdom: VdomWeak, id: usize);
+
+    /// Update the edits for the todo with the given id.
+    fn update_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize, edits: String);
+
+    /// Finish editing the todo with the given id.
+    fn finish_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize);
+
+    /// Cancel editing the todo with the given id.
+    fn cancel_edits(root: &mut dyn RootRender, vdom: VdomWeak, id: usize);
+}
+
+impl<C> Todo<C> {
+    /// Construct a new `Todo` with the given identifier and title.
+    pub fn new<S: Into<String>>(id: usize, title: S) -> Self {
+        let title = title.into();
+        let completed = false;
+        let edits = None;
+        Todo {
+            id,
+            title,
+            completed,
+            edits,
+            _controller: PhantomData,
+        }
+    }
+
+    /// Set this todo item's id.
+    pub fn set_id(&mut self, id: usize) {
+        self.id = id;
+    }
+
+    /// Is this `Todo` complete?
+    pub fn is_complete(&self) -> bool {
+        self.completed
+    }
+
+    /// Mark the `Todo` as complete or not.
+    pub fn set_complete(&mut self, to: bool) {
+        self.completed = to;
+    }
+
+    /// Get this todo's title.
+    pub fn title(&self) -> &str {
+        &self.title
+    }
+
+    /// Set this todo item's title.
+    pub fn set_title<S: Into<String>>(&mut self, title: S) {
+        self.title = title.into();
+    }
+
+    /// Set the edits for this todo.
+    pub fn set_edits<S: Into<String>>(&mut self, edits: Option<S>) {
+        self.edits = edits.map(Into::into);
+    }
+
+    /// Take this todo's edits, leaving `None` in their place.
+    pub fn take_edits(&mut self) -> Option<String> {
+        self.edits.take()
+    }
+}
+
+impl<C: TodoActions> Render for Todo<C> {
+    fn render<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
+        use dodrio::{
+            builder::*,
+            bumpalo::{self, collections::String},
+        };
+
+        let id = self.id;
+        let title = self.edits.as_ref().unwrap_or(&self.title);
+        let title = bumpalo::format!(in cx.bump, "{}", title).into_bump_str();
+
+        li(&cx)
+            .attr("class", {
+                let mut class = String::new_in(cx.bump);
+                if self.completed {
+                    class.push_str("completed ");
+                }
+                if self.edits.is_some() {
+                    class.push_str("editing");
+                }
+                class.into_bump_str()
+            })
+            .children([
+                div(&cx)
+                    .attr("class", "view")
+                    .children([
+                        input(&cx)
+                            .attr("class", "toggle")
+                            .attr("type", "checkbox")
+                            .bool_attr("checked", self.completed)
+                            .on("click", move |root, vdom, _event| {
+                                C::toggle_completed(root, vdom, id);
+                            })
+                            .finish(),
+                        label(&cx)
+                            .on("dblclick", move |root, vdom, _event| {
+                                C::begin_editing(root, vdom, id);
+                            })
+                            .children([text(title)])
+                            .finish(),
+                        button(&cx)
+                            .attr("class", "destroy")
+                            .on("click", move |root, vdom, _event| {
+                                C::delete(root, vdom, id);
+                            })
+                            .finish(),
+                    ])
+                    .finish(),
+                input(&cx)
+                    .attr("class", "edit")
+                    .attr("value", title)
+                    .attr("name", "title")
+                    .attr(
+                        "id",
+                        bumpalo::format!(in cx.bump, "todo-{}", id).into_bump_str(),
+                    )
+                    .on("input", move |root, vdom, event| {
+                        let input = event
+                            .target()
+                            .unwrap_throw()
+                            .unchecked_into::<web_sys::HtmlInputElement>();
+                        C::update_edits(root, vdom, id, input.value());
+                    })
+                    .on("blur", move |root, vdom, _event| {
+                        C::finish_edits(root, vdom, id);
+                    })
+                    .on("keydown", move |root, vdom, event| {
+                        let event = event.unchecked_into::<web_sys::KeyboardEvent>();
+                        match event.key_code() {
+                            keys::ENTER => C::finish_edits(root, vdom, id),
+                            keys::ESCAPE => C::cancel_edits(root, vdom, id),
+                            _ => {}
+                        }
+                    })
+                    .finish(),
+            ])
+            .finish()
+    }
+}
---
+++ src/todos.rs
@@ -0,0 +1,301 @@
+//! Type definitions and `dodrio::Render` implementation for a collection of
+//! todo items.
+
+use crate::controller::Controller;
+use crate::todo::{Todo, TodoActions};
+use crate::visibility::Visibility;
+use crate::{keys, utils};
+use dodrio::{bumpalo, Node, Render, RenderContext, RootRender, VdomWeak};
+use serde::{Deserialize, Serialize};
+use std::marker::PhantomData;
+use std::mem;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+/// A collection of todos.
+#[derive(Default, Serialize, Deserialize)]
+#[serde(rename = "todos-dodrio", bound = "")]
+pub struct Todos<C = Controller> {
+    todos: Vec<Todo<C>>,
+
+    #[serde(skip)]
+    draft: String,
+
+    #[serde(skip)]
+    visibility: Visibility,
+
+    #[serde(skip)]
+    _controller: PhantomData<C>,
+}
+
+/// Actions for `Todos` that can be triggered by UI interactions.
+pub trait TodosActions: TodoActions {
+    /// Toggle the completion state of all todo items.
+    fn toggle_all(root: &mut dyn RootRender, vdom: VdomWeak);
+
+    /// Update the draft todo item's text.
+    fn update_draft(root: &mut dyn RootRender, vdom: VdomWeak, draft: String);
+
+    /// Finish the current draft todo item and add it to the collection of
+    /// todos.
+    fn finish_draft(root: &mut dyn RootRender, vdom: VdomWeak);
+
+    /// Change the todo item visibility filtering to the given `Visibility`.
+    fn change_visibility(root: &mut dyn RootRender, vdom: VdomWeak, vis: Visibility);
+
+    /// Delete all completed todo items.
+    fn delete_completed(root: &mut dyn RootRender, vdom: VdomWeak);
+}
+
+impl<C> Todos<C> {
+    /// Construct a new todos set.
+    ///
+    /// If an existing set is available in local storage, then us that,
+    /// otherwise create a new set.
+    pub fn new() -> Self
+    where
+        C: Default,
+    {
+        Self::from_local_storage().unwrap_or_default()
+    }
+
+    /// Deserialize a set of todos from local storage.
+    pub fn from_local_storage() -> Option<Self> {
+        utils::local_storage()
+            .get("todomvc-dodrio")
+            .ok()
+            .and_then(|opt| opt)
+            .and_then(|json| serde_json::from_str(&json).ok())
+    }
+
+    /// Serialize this set of todos to local storage.
+    pub fn save_to_local_storage(&self) {
+        let serialized = serde_json::to_string(self).unwrap_throw();
+        utils::local_storage()
+            .set("todomvc-dodrio", &serialized)
+            .unwrap_throw();
+    }
+
+    /// Add a new todo item to this collection.
+    pub fn add_todo(&mut self, todo: Todo<C>) {
+        self.todos.push(todo);
+    }
+
+    /// Delete the todo with the given id.
+    pub fn delete_todo(&mut self, id: usize) {
+        self.todos.remove(id);
+        self.fix_ids();
+    }
+
+    /// Delete all completed todo items.
+    pub fn delete_completed(&mut self) {
+        self.todos.retain(|t| !t.is_complete());
+        self.fix_ids();
+    }
+
+    // Fix all todo identifiers so that they match their index once again.
+    fn fix_ids(&mut self) {
+        for (id, todo) in self.todos.iter_mut().enumerate() {
+            todo.set_id(id);
+        }
+    }
+
+    /// Get a shared slice of the underlying set of todo items.
+    pub fn todos(&self) -> &[Todo<C>] {
+        &self.todos
+    }
+
+    /// Get an exclusive slice of the underlying set of todo items.
+    pub fn todos_mut(&mut self) -> &mut [Todo<C>] {
+        &mut self.todos
+    }
+
+    /// Set the draft todo item text.
+    pub fn set_draft<S: Into<String>>(&mut self, draft: S) {
+        self.draft = draft.into();
+    }
+
+    /// Take the current draft text and replace it with an empty string.
+    pub fn take_draft(&mut self) -> String {
+        mem::replace(&mut self.draft, String::new())
+    }
+
+    /// Get the current visibility for these todos.
+    pub fn visibility(&self) -> Visibility {
+        self.visibility
+    }
+
+    /// Set the visibility for these todoS.
+    pub fn set_visibility(&mut self, vis: Visibility) {
+        self.visibility = vis;
+    }
+}
+
+/// Rendering helpers.
+impl<C: TodosActions> Todos<C> {
+    fn header<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
+        use dodrio::builder::*;
+
+        header(&cx)
+            .attr("class", "header")
+            .children([
+                h1(&cx).children([text("todos")]).finish(),
+                input(&cx)
+                    .on("input", |root, vdom, event| {
+                        let input = event
+                            .target()
+                            .unwrap_throw()
+                            .unchecked_into::<web_sys::HtmlInputElement>();
+                        C::update_draft(root, vdom, input.value());
+                    })
+                    .on("keydown", |root, vdom, event| {
+                        let event = event.unchecked_into::<web_sys::KeyboardEvent>();
+                        if event.key_code() == keys::ENTER {
+                            C::finish_draft(root, vdom);
+                        }
+                    })
+                    .attr("class", "new-todo")
+                    .attr("placeholder", "What needs to be done?")
+                    .attr("autofocus", "")
+                    .attr(
+                        "value",
+                        bumpalo::format!(in cx.bump, "{}", self.draft).into_bump_str(),
+                    )
+                    .finish(),
+            ])
+            .finish()
+    }
+
+    fn todos_list<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
+        use dodrio::{builder::*, bumpalo::collections::Vec};
+
+        let mut todos = Vec::with_capacity_in(self.todos.len(), cx.bump);
+        todos.extend(
+            self.todos
+                .iter()
+                .filter(|t| match self.visibility {
+                    Visibility::All => true,
+                    Visibility::Active => !t.is_complete(),
+                    Visibility::Completed => t.is_complete(),
+                })
+                .map(|t| t.render(cx)),
+        );
+
+        section(&cx)
+            .attr("class", "main")
+            .attr(
+                "visibility",
+                if self.todos.is_empty() {
+                    "hidden"
+                } else {
+                    "visible"
+                },
+            )
+            .children([
+                input(&cx)
+                    .attr("class", "toggle-all")
+                    .attr("id", "toggle-all")
+                    .attr("type", "checkbox")
+                    .attr("name", "toggle")
+                    .bool_attr("checked", self.todos.iter().all(|t| t.is_complete()))
+                    .on("click", |root, vdom, _event| {
+                        C::toggle_all(root, vdom);
+                    })
+                    .finish(),
+                label(&cx)
+                    .attr("for", "toggle-all")
+                    .children([text("Mark all as complete")])
+                    .finish(),
+                ul(&cx).attr("class", "todo-list").children(todos).finish(),
+            ])
+            .finish()
+    }
+
+    fn footer<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
+        use dodrio::builder::*;
+
+        let completed_count = self.todos.iter().filter(|t| t.is_complete()).count();
+        let incomplete_count = self.todos.len() - completed_count;
+        let items_left = if incomplete_count == 1 {
+            " item left"
+        } else {
+            " items left"
+        };
+        let incomplete_count = bumpalo::format!(in cx.bump, "{}", incomplete_count);
+
+        let clear_completed_text = bumpalo::format!(
+            in cx.bump,
+            "Clear completed ({})",
+            self.todos.iter().filter(|t| t.is_complete()).count()
+        );
+
+        footer(&cx)
+            .attr("class", "footer")
+            .bool_attr("hidden", self.todos.is_empty())
+            .children([
+                span(&cx)
+                    .attr("class", "todo-count")
+                    .children([
+                        strong(&cx)
+                            .children([text(incomplete_count.into_bump_str())])
+                            .finish(),
+                        text(items_left),
+                    ])
+                    .finish(),
+                ul(&cx)
+                    .attr("class", "filters")
+                    .children([
+                        self.visibility_swap(cx, "#/", Visibility::All),
+                        self.visibility_swap(cx, "#/active", Visibility::Active),
+                        self.visibility_swap(cx, "#/completed", Visibility::Completed),
+                    ])
+                    .finish(),
+                button(&cx)
+                    .on("click", |root, vdom, _event| {
+                        C::delete_completed(root, vdom);
+                    })
+                    .attr("class", "clear-completed")
+                    .bool_attr("hidden", completed_count == 0)
+                    .children([text(clear_completed_text.into_bump_str())])
+                    .finish(),
+            ])
+            .finish()
+    }
+
+    fn visibility_swap<'a>(
+        &self,
+        cx: &mut RenderContext<'a>,
+        url: &'static str,
+        target_vis: Visibility,
+    ) -> Node<'a> {
+        use dodrio::builder::*;
+
+        li(&cx)
+            .on("click", move |root, vdom, _event| {
+                C::change_visibility(root, vdom, target_vis);
+            })
+            .children([a(&cx)
+                .attr("href", url)
+                .attr(
+                    "class",
+                    if self.visibility == target_vis {
+                        "selected"
+                    } else {
+                        ""
+                    },
+                )
+                .children([text(target_vis.label())])
+                .finish()])
+            .finish()
+    }
+}
+
+impl<C: TodosActions> Render for Todos<C> {
+    fn render<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
+        use dodrio::builder::*;
+
+        div(&cx)
+            .children([self.header(cx), self.todos_list(cx), self.footer(cx)])
+            .finish()
+    }
+}
